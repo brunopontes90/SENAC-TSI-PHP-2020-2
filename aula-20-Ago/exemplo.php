9da23257d5d3ced6b5d0bc87fbed209ddb605175
Bruno Pontes <bruno_confessor@hotmail.com>
2020-08-20 22:48:55 -0300

    Exemplo de operadores rodando já com servidor web

---
+++ aula-20-Ago/exemplo.php
@@ -0,0 +1,17 @@
+<?php
+// COMENTARIO DE LINHA
+
+$saldo = 34.5;
+
+// ASPAS DUPLAS ACEITAM STRING ESPECIAL
+echo("<br><br><br>Seu saldo é $saldo"); // OPERADOR DE ATRIBUIÇÃO
+
+// ASPAS SIMPLES APENAS PURA STRING
+echo('<br><br><br>Seu saldo é $saldo');
+
+$saldo -= 10; // OPERADOR DE SUBTRAÇÃO
+echo("<br><br><br>Seu saldo é $saldo");
+
+
+$saldo *= 1000; // OPERADOR DE MULTIPLICAÇÃO
+echo("<br><br><br>Seu saldo é $saldo");
